5c53a94cafe17d54dae2d25857e965c4cfa771f3
Alec Resende <universalsequences@gmail.com>
2023-01-19 20:50:22 -0500

    build

---
+++ precache-manifest.9c34c066e221f7191a798ccc97995aa6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b7995613c93192e9ff5",
+    "url": "/static/css/main.f27f1acb.chunk.css"
+  },
+  {
+    "revision": "5b7995613c93192e9ff5",
+    "url": "/static/js/main.1501f75c.chunk.js"
+  },
+  {
+    "revision": "a45aad5e5bf6ac4c24e6",
+    "url": "/static/js/runtime~main.a4b745b8.js"
+  },
+  {
+    "revision": "85d2d08b8986265dbcdb",
+    "url": "/static/css/2.3d93e49e.chunk.css"
+  },
+  {
+    "revision": "85d2d08b8986265dbcdb",
+    "url": "/static/js/2.b8351476.chunk.js"
+  },
+  {
+    "revision": "3c729868c7141e4eadd4",
+    "url": "/static/css/3.c126c183.chunk.css"
+  },
+  {
+    "revision": "3c729868c7141e4eadd4",
+    "url": "/static/js/3.427557d8.chunk.js"
+  },
+  {
+    "revision": "19439c5db1bfd3a55e05",
+    "url": "/static/js/4.452e066b.chunk.js"
+  },
+  {
+    "revision": "e9a2cb07f00d9b1bd4f6a2bf5f57313e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba7202c14bb5ee678e52f23ad5d6a9cc.js"
+  "/precache-manifest.9c34c066e221f7191a798ccc97995aa6.js"
 );
 
 workbox.clientsClaim();
